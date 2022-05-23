0880c537b4b4051db813216600ba3aeaed62eb84
RakaRyG <rakaguntara10@gmail.com>
2022-05-23 08:33:38 +0700

    profil1

--- 2. Slink UAS/src/profil1.php
+++ 2. Slink UAS/src/profil1.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html lang="en">
 
-<?php require "./function.php"; 
+<?php
+require "./function.php";
 ?>
 
 <head>
@@ -9,70 +10,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>profil</title>
     <link rel="stylesheet" href="../css/style.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <!-- navugasi -->
 <div class="animasi2">
-    <nav class="navbar navbar-expand-lg bg-white shadow-sm p-3">
-        <div class="container-fluid">
-            <a class="navbar-brand ms-5" href="../index.php"><img src="../Foto/logo.png" alt="" width="105" /></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cms</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About as</a>
-                    </li>
-                </ul>
-                </ul>
-                <img src="../Foto/raka.jpg" class="rounded-circle me-3" width="40px">
+    <?php
+    include("./include/navbar.php");
+    ?>
 
-                <?php 
-                $user_id = $_SESSION['user_id'];
-                $query = "SELECT * FROM users WHERE id = $user_id";
-                $select_user = mysqli_query($conn, $query);
+    <!-- Query profile -->
+    <?php
+    $user_id = $_SESSION['user_id'];
 
-                while($row = mysqli_fetch_assoc($select_user)){
-                    $id = $row['id'];
-                    $nama = $row['nama'];
-                    $foto = $row['foto'];
-                    $email = $row['email'];
-                    
-                }
-                
-                
-                ?>
-                <a class="nav-link me-5" href="./home.php">Raka Guntara</a>
+    $query = "SELECT * FROM users WHERE id = $user_id";
+    $get_profile = mysqli_query($conn, $query);
 
-                <!-- <button class=" btn " type=" button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
-                    aria-controls="offcanvasRight"></button> -->
-            </div>
-        </div>
-    </nav>
+    while ($row = mysqli_fetch_assoc($get_profile)) {
+        $nama = $row['nama'];
+        $username = $row['username'];
+        $foto = $row['foto'];
+        $email = $row['email'];
+        $bio = $row['bio'];
+        $date = $row['waktu_bergabung'];
+
+        if ($foto == "" || empty($foto) || $foto == null) {
+            $tag = "<img class='rounded-circle mt-5' width='150px' src='../Foto/user.png'><span class=' font-weight-bold'>$foto</span>";
+        } else {
+            $tag = "<img class='rounded-circle mt-5' width='150px' src='../Foto/$foto'><span class=' font-weight-bold'>$foto</span>";
+        }
+    }
 
 
+    ?>
+
+    <!-- Query update -->
+    <?php
+    if (isset($_POST['save_profile'])) {
+        $name = $_POST['name'];
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $bio = $_POST['bio'];
+
+        $query = "UPDATE users SET name = '{$name}', ";
+        $query .= "SET username = '{$username}', ";
+        $query .= "SET email = '{$email}', ";
+        $query .= "SET bio = '{$bio}' WHERE id = $user_id ";
+
+        $update_user = mysqli_query($conn, $query);
+    }
+
+    ?>
 
 
     <!-- profil -->
     <div class="container-xl ">
-        <div class="profil ">
+        <form class="profil " action="" method="post">
             <div class="container rounded bg-white ms-5 mt-5 mb-5">
                 <div class="row">
                     <div class="col-md-3 border-right">
-                        <div
-                            class="d-flex flex-column align-items-center text-center p-3 py-5 shadow-sm p-3 mb-5 bg-body rounded">
-                            <img class="rounded-circle mt-5" width="150px" src="../Foto/raka.jpg"><span
-                                class=" font-weight-bold">Raka Guntara</span>
+                        <div class="d-flex flex-column align-items-center text-center p-3 py-5 shadow-sm p-3 mb-5 bg-body rounded">
+                            <!-- Foto -->
+                            <?php echo $tag; ?>
                             <br>
                             <span>
                                 <div class="row">
@@ -95,19 +93,15 @@
                                 <h4 class="text-right">Profile Settings</h4>
                             </div>
                             <div class="row mt-2">
-                                <div class="col-md-12"><label class="labels">Name</label><input type="text"
-                                        class="form-control" placeholder="first name" value=""></div>
+                                <div class="col-md-12"><label class="labels">Name</label><input type="text" class="form-control" name="name" placeholder="full name" value="<?php echo $nama; ?>"></div>
                             </div>
                             <div class="row mt-2">
-                                <div class="col-md-12"><label class="labels">Username</label><input type="text"
-                                        class="form-control" placeholder="enter your email" value=""></div>
-                                <div class="col-md-12"><label class="labels">Email </label><input type="text"
-                                        class="form-control" placeholder="enter address line " value=""></div>
+                                <div class="col-md-12"><label class="labels">Username</label><input type="text" name="username" class="form-control" placeholder="enter your email" value="<?php echo $username; ?>"></div>
+                                <div class="col-md-12"><label class="labels">Email </label><input type="text" name="email" class="form-control" placeholder="enter address line " value="<?php echo $email; ?>"></div>
                             </div>
                             <div class="row mt-2">
                                 <label for="floatingTextarea">Bio</label>
-                                <div class="col-md-12"><textarea class="form-control" placeholder="Leave a comment here"
-                                        id="floatingTextarea"></textarea>
+                                <div class="col-md-12"><textarea class="form-control" name="bio" placeholder="Leave a comment here" id="floatingTextarea"><?php echo $bio; ?></textarea>
 
                                 </div>
                             </div>
@@ -118,23 +112,21 @@
                         <div class="p-3 py-5">
                             <br><br>
                             <div class="col-md-12"><label class="labels">Waktu Bergabung</label><br>
-                                <span class="fs-3">10-10-2022</span>
+                                <span class="fs-3"><?php echo $date; ?></span>
                                 <br><br>
-                                <div class=" col-md-12"><button class="btn btn-success profile-button " type="button"
-                                        style="">Save
-                                        Profile</button><button type="button"
-                                        class="btn btn-outline-danger mx-3">Logout</button>
+                                <div class=" col-md-12">
+                                    <button class="btn btn-success profile-button" name="save_profile" type="submit" style="">Save Profile</button>
+                                    <button type="button" class="btn btn-outline-danger mx-3">Logout</button>
                                 </div>
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
+        </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
     </body>
 
