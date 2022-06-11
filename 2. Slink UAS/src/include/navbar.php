3640165ec3d539f9bd7ec243e49dda6e9c09eb0d
Dhafin Taufiqi <101368249+Dhafin2107@users.noreply.github.com>
2022-06-11 14:12:41 +0700

    ganti warna di navbar

--- 2. Slink UAS/src/include/navbar.php
+++ 2. Slink UAS/src/include/navbar.php
@@ -3,22 +3,24 @@
             <nav class="navbar navbar-expand-lg bg-white shadow-sm p-3">
                 <div class="container-fluid">
                     <a class="navbar-brand ms-5" href="./home.php"><img src="../Foto/logo.png" alt="" width="105" /></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link text-primary" aria-current="page" href="./home.php">Home</a>
+                                <a class="nav-link text-secondary" aria-current="page" href="./home.php">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-primary" href="./cms.php">Links</a>
+                                <a class="nav-link text-secondary" href="./cms.php">Links</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-primary" href="./bookmarks.php">Bookmarks</a>
+                                <a class="nav-link text-secondary" href="./bookmarks.php">Bookmarks</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-primary" href="./leaderboard.php">Leaderboard</a>
+                                <a class="nav-link text-secondary" href="./leaderboard.php ">Leaderboard</a>
                             </li>
                         </ul>
                         </ul>
@@ -36,8 +38,9 @@
                         }
                         ?>
 
-                        <a class="nav-link me-5" href="./profile.php"><?php echo $nama; ?></a>
-                        <a class="nav-link me-5 text-danger" href="./cms.php?logout=<?= true ?>" id="logout">Log Out</a>
+                        <a class=" nav-link me-5 text-secondary" href="./profile.php"><?php echo $nama; ?></a>
+                        <a class="nav-link me-5 text-danger" href="./cms.php?logout=<?= true ?>" id="logout">Log
+                            Out</a>
                     </div>
                 </div>
             </nav>
